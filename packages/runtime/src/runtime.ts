8ca86b7ff2198d2d074f8d0396dc6925ebe091e3
Joe Clark <josephjclark@gmail.com>
2022-08-23 17:54:25 +0100

    Preserve state before each operation)

--- packages/runtime/src/runtime.ts
+++ packages/runtime/src/runtime.ts
@@ -1,4 +1,5 @@
 import type { Operation, State } from '@openfn/language-common';
+import { json } from 'stream/consumers';
 
 type Obj = Record<string, unknown>;
 
@@ -7,6 +8,11 @@ const defaultState = { data: {}, configuration: {} };
 // type Opts = {
 //   logger: any; // path to module or an actual object?
 // }
+
+// TODO I'm in the market for the best solution here
+// immer? deep-clone?
+// What should we do if functions are in the state?
+const clone = (state: State) => JSON.parse(JSON.stringify(state))
 
 // Copied from language-common
 export function execute(...operations: Array<Operation>): Operation {
@@ -19,14 +25,20 @@ export function execute(...operations: Array<Operation>): Operation {
   };
 }
 
+const wrapFn = (fn: Operation) => {
+  return (state: State) => {
+    const newState = clone(state);
+    return fn(newState);
+  }
+};
+
 
 // this is presumably async?
 async function run(jobs: Operation[], initialState: State = defaultState) {
   // Can I re-use execute or do I need to write my own?
   // nice to prove the principal with the core loop...
   // TODO how do we inject into state?
-  console.log('run')
-  const reducer = execute(...jobs);
+  const reducer = execute(...jobs.map(wrapFn));
   const result = await reducer(initialState);
   return result;
 }
--- packages/runtime/test/runtime.test.ts
+++ packages/runtime/test/runtime.test.ts
@@ -1,7 +1,13 @@
 import test from "ava";
 import { fn } from '@openfn/language-common';
-import type { State } from '@openfn/language-common';
+import type { State, Operation } from '@openfn/language-common';
 import run from '../src/runtime';
+
+type TestState = State & {
+  data: {
+    x: number
+  }
+};
 
 const createState = (data = {}) => ({
   data: data,
@@ -14,7 +20,7 @@ test('a no-op job with one operation', async (t) => {
   const result = await run(job, state);
 
   t.deepEqual(state, result);
-})
+});
 
 test('a no-op job with one fn operation', async (t) => {
   const job = [fn((s: State) => s)];
@@ -22,8 +28,78 @@ test('a no-op job with one fn operation', async (t) => {
   const result = await run(job, state);
 
   t.deepEqual(state, result);
+});
+
+test('jobs can handle a promise', async (t) => {
+  const job = [async (s: State) => s];
+  const state = createState();
+  const result = await run(job, state);
+
+  t.deepEqual(state, result);
+});
+
+test('jobs run in series', async (t) => {
+  const job = [
+    (s: TestState) => {
+      s.data.x = 2
+      return s;
+    },
+    (s: TestState) => {
+      s.data.x += 2;
+      return s;
+    },
+    (s: TestState) => {
+      s.data.x *= 3;
+      return s;
+    }
+  ] as Operation[];
+
+  const state = createState();
+  // @ts-ignore
+  t.falsy(state.data.x);
+
+  const result = await run(job, state) as TestState;
+
+  t.is(result.data.x, 12);
 })
 
-// test('should call the logger', () => {
-  
-// })
+test('jobs run in series with async operations', async (t) => {
+  const job = [
+    (s: TestState) => {
+      s.data.x = 2
+      return s;
+    },
+    (s: TestState) => new Promise(resolve => {
+      setTimeout(() => {
+        s.data.x += 2;
+        resolve(s)
+      }, 10);
+    }),
+    (s: TestState) => {
+      s.data.x *= 3;
+      return s;
+    }
+  ] as Operation[];
+
+  const state = createState();
+  // @ts-ignore
+  t.falsy(state.data.x);
+
+  const result = await run(job, state) as TestState;
+
+  t.is(result.data.x, 12);
+})
+
+
+test('jobs do not mutate the original state', async (t) => {
+  const job = [(s: TestState) => {
+    s.data.x = 2;
+    return s;
+  }] as Operation[];
+
+  const state = createState({ x: 1 }) as TestState;
+  const result = await run(job, state) as TestState;
+
+  t.is(state.data.x, 1);
+  t.is(result.data.x, 2);
+})
