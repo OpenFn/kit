f62572d984f8e97f7de6fc9de32729713ff90ffb
Joe Clark <josephjclark@gmail.com>
2022-08-31 12:56:38 +0100

    Update and fix build

--- packages/compiler/src/compile.ts
+++ packages/compiler/src/compile.ts
@@ -1,22 +1,22 @@
+import { print } from 'recast';
+import parse from './parse';
+import transform from './transform';
+import { loadFile } from './utils';
+
+type Options = {
+  // Evaluate the input path as source, rather than loading from disk
+  // TODO not keen on the name of this
+  // Can we not detect a file name? No line breaks, ends in js or maybe ojs
+  eval?: boolean;
+}
+
 // This is the main compiler function
 // Load source from a file
 // parse it
 // transform it
 // write it (with recast)
-
-import { loadFile } from './utils';
-import { print } from 'recast';
-
-import parse from './parse';
-import transform from './transform';
-
-type Options = {
-  // Evaluate the input path as source, rather than loading from disk
-  eval?: boolean;
-}
-
 // TODO should this be async? not sure yet there's any need
-export default function compile(path: string, opts: Options) {
+export default function compile(path: string, opts: Options = {}) {
   const source = opts.eval ? path : loadFile(path);
 
   const ast = parse(source);
--- packages/compiler/src/utils.ts
+++ packages/compiler/src/utils.ts
@@ -1,4 +1,4 @@
 import fs from 'node:fs';
 import path from 'node:path';
 
-export const loadFile = (filePath: string) => fs.readFileSync(path.resolve(filePath));
+export const loadFile = (filePath: string) => fs.readFileSync(path.resolve(filePath), 'utf8');
