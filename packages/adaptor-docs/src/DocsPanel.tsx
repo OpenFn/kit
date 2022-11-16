5520f8e7bb822be03b1f44d3aac08387d4ffd178
Joe Clark <josephjclark@gmail.com>
2022-11-16 18:44:16 +0000

    adaptor-docs: Hooked up copy & insert buttons

--- packages/adaptor-docs/src/DocsPanel.tsx
+++ packages/adaptor-docs/src/DocsPanel.tsx
@@ -7,6 +7,7 @@ import useDocs from './useDocs';
 
 type DocsPanelProps = {
   specifier?: string;
+  onInsert?: (text: string) => void;
 }
 
 const getSignature = (fn: FunctionDescription) => {
@@ -21,14 +22,26 @@ const getSignature = (fn: FunctionDescription) => {
 // - it has to be collapsible
 // - it needs a lot more metadata (args, example)
 
-const PreButton = ({ label }) => 
+const PreButton = ({ label, onClick }: { label: string, onClick?: () => void }) => 
+  // TODO give some kind of feedback on click
   <button
     className="rounded-md bg-slate-300 text-white px-2 py-1 mr-1 text-xs"
-    onClick={() => alert('not implemented yet')}>
+    onClick={onClick}>
     {label}
   </button>
 
-const DocsFn = ({ item }: { item: FunctionDescription }) => {
+const DocsFn = ({ item, onInsert }: { item: FunctionDescription, onInsert?: DocsPanelProps['onInsert'] }) => {
+  const doCopy = async (text: string) => {
+    const type = "text/plain";
+    const data = [new ClipboardItem({ [type]: new Blob([text], { type } )})];
+  
+    try {
+      await navigator.clipboard.write(data);
+    } catch(e) {
+      alert('COPY FAILED')
+    }
+  }
+
   return (
     <div className="block mb-10">
       <label className="block text-m font-bold text-secondary-700 mb-1">{getSignature(item)}</label>
@@ -36,8 +49,8 @@ const DocsFn = ({ item }: { item: FunctionDescription }) => {
       {item.examples.map((eg, idx) =>
         <>
           <div className="w-full px-5 text-right" style={{ height: '13px'}}>
-            <PreButton label="COPY" />
-            <PreButton label="INSERT" />
+            <PreButton label="COPY" onClick={() => doCopy(eg)} />
+            {onInsert && <PreButton label="INSERT" onClick={() => onInsert(eg)} />}
           </div>
           <pre
             className="rounded-md pl-4 pr-30 py-2 mx-4 my-0 font-mono bg-slate-100 border-2 border-slate-200 text-slate-800 min-h-full text-xs overflow-x-auto"
@@ -51,7 +64,7 @@ const DocsFn = ({ item }: { item: FunctionDescription }) => {
 }
 
 
-const DocsPanel = ({ specifier }: DocsPanelProps) => {
+const DocsPanel = ({ specifier, onInsert }: DocsPanelProps) => {
   if (!specifier) {;
     return <div>nothing selected</div>;
   }
@@ -61,7 +74,7 @@ const DocsPanel = ({ specifier }: DocsPanelProps) => {
   return (
     <div className="block m-2 mb-8">
       <h1 className="h1 text-xl mb-6 font-bold text-secondary-700">{name} v{version}</h1>
-      {docs.map((item) => <DocsFn key={item.name} item={item} />)}
+      {docs.map((item) => <DocsFn key={item.name} item={item} onInsert={onInsert} />)}
     </div>
     );
 };
