8aed7dbd174b26ca78107578c382856e415073af
Joe Clark <josephjclark@gmail.com>
2022-11-24 16:43:18 +0000

    cli: ignore autoinstall adaptors with paths

---
+++ packages/cli/test/execute/autoinstall-targets.test.ts
@@ -0,0 +1,88 @@
+import test from 'ava';
+
+import { getAutoinstallTargets } from '../../src/execute/handler';
+
+test('return empty if autoinstall is false', (t) => {
+  const result = getAutoinstallTargets({
+    autoinstall: false,
+    adaptors: ['a', 'b'],
+  });
+  t.truthy(result);
+  t.is(result.length, 0);
+});
+
+test('return empty if an empty array is passed', (t) => {
+  const result = getAutoinstallTargets({
+    autoinstall: true,
+    adaptors: [],
+  });
+  t.truthy(result);
+  t.is(result.length, 0);
+});
+
+test('return 2 valid targets', (t) => {
+  const result = getAutoinstallTargets({
+    autoinstall: true,
+    adaptors: ['a', 'b'],
+  });
+  t.truthy(result);
+  t.is(result.length, 2);
+  t.deepEqual(result, ['a', 'b']);
+});
+
+test('return empty if a path is passed', (t) => {
+  const result = getAutoinstallTargets({
+    autoinstall: true,
+    adaptors: ['a=a/b/c'],
+  });
+  t.truthy(result);
+  t.is(result.length, 0);
+});
+
+test('return 1 valid target', (t) => {
+  const result = getAutoinstallTargets({
+    autoinstall: true,
+    adaptors: ['a=/some/path', 'b@1.2.3'],
+  });
+  t.truthy(result);
+  t.is(result.length, 1);
+  t.deepEqual(result, ['b@1.2.3']);
+});
+
+test('return language common', (t) => {
+  const result = getAutoinstallTargets({
+    autoinstall: true,
+    adaptors: ['@openfn/language-common'],
+  });
+  t.truthy(result);
+  t.is(result.length, 1);
+  t.deepEqual(result, ['@openfn/language-common']);
+});
+
+test('return language common with specifier', (t) => {
+  const result = getAutoinstallTargets({
+    autoinstall: true,
+    adaptors: ['@openfn/language-common@1.0.0'],
+  });
+  t.truthy(result);
+  t.is(result.length, 1);
+  t.deepEqual(result, ['@openfn/language-common@1.0.0']);
+});
+
+test('reject language common with path', (t) => {
+  const result = getAutoinstallTargets({
+    autoinstall: true,
+    adaptors: ['@openfn/language-common=/a/b/c'],
+  });
+  t.truthy(result);
+  t.is(result.length, 0);
+});
+
+test('reject language common with specifier and path', (t) => {
+  const result = getAutoinstallTargets({
+    autoinstall: true,
+    adaptors: ['@openfn/language-common@1.0.0=/tmp/repo/common'],
+  });
+  t.truthy(result);
+  t.is(result.length, 0);
+});
