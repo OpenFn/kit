bcec9ec3305f88e9501244fb4b73ba1fe0c1feb4
Joe Clark <josephjclark@gmail.com>
2023-01-06 14:26:24 +0000

    integration-tests: Setup sets to use local or global openfn command (depending on env)

--- packages/integration-tests/src/cli.test.ts
+++ packages/integration-tests/src/cli.test.ts
@@ -1,5 +1,18 @@
 import test from 'ava';
+import run from './run';
 
-test('it works', (t) => {
-  t.true(true);
+test.serial('version', async (t) => {
+  const { stdout } = await run('version');
+  t.regex(stdout, /Versions/);
+  t.regex(stdout, /Node.js/);
+  t.regex(stdout, /cli/);
+  t.regex(stdout, /runtime/);
+  t.regex(stdout, /compiler/);
+});
+
+test.serial('test', async (t) => {
+  const { stdout } = await run('test');
+  t.regex(stdout, /Versions:/);
+  t.regex(stdout, /Running test job.../);
+  t.regex(stdout, /Result: 42/);
 });
