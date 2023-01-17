508509436fc7b6774ae0efb99362cebbdeec69e4
Joe Clark <josephjclark@gmail.com>
2023-01-17 12:10:45 +0000

    logger: remove redundant line

--- packages/logger/src/logger.ts
+++ packages/logger/src/logger.ts
@@ -147,8 +147,6 @@ export default function (name?: string, options: LogOptions = {}): Logger {
   };
 
   const logString = (level: LogFns, ...args: LogArgs) => {
-    if (opts.level === NONE) return;
-
     if (emitter.hasOwnProperty(level)) {
       const output = [];
 
