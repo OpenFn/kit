f7b676b2ca4ad97ecff4f36eb1b13c5146d24b6a
Joe Clark <jclark@openfn.org>
2023-02-08 08:49:10 +0200

    cli: don't log when expanding adaptors
    It's not actually that helpful (especially if we print out the adaptor version), and the requirement for a logger before options have been parsed is problematic

--- packages/cli/src/util/expand-adaptors.ts
+++ packages/cli/src/util/expand-adaptors.ts
@@ -1,13 +1,7 @@
-import { createNullLogger } from './logger';
-
-const nullLogger = createNullLogger();
-
-export default (names: string[], log = nullLogger) =>
+export default (names: string[]) =>
   names?.map((name) => {
     if (name.startsWith('@openfn/language-')) {
       return name;
     }
-    const expanded = `@openfn/language-${name}`;
-    log.debug(`Expanded adaptor ${name} to ${expanded}`);
-    return expanded;
+    return `@openfn/language-${name}`;
   });
