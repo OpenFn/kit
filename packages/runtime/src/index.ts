596b9c733fab20d75eeaa92de74b61d0248d8bec
Joe Clark <josephjclark@gmail.com>
2023-04-12 17:04:07 +0100

    runtime: export new type definitions

--- packages/runtime/src/index.ts
+++ packages/runtime/src/index.ts
@@ -4,4 +4,6 @@ export default run;
 import type { ModuleInfo, ModuleInfoMap } from './modules/linker';
 export type { ModuleInfo, ModuleInfoMap };
 
+export * from './types';
+
 export * from './modules/repo';
