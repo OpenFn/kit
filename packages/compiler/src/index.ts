5519e1be59c98f19dfae3fe5f7cae500e64dbd05
Joe Clark <josephjclark@gmail.com>
2023-04-19 16:22:56 +0100

    compiler: export more utility functions

--- packages/compiler/src/index.ts
+++ packages/compiler/src/index.ts
@@ -1,6 +1,6 @@
 import compile from './compile';
 
-export { preloadAdaptorExports } from './util';
+export * from './util';
 export type { TransformOptions } from './transform';
 export type { Options } from './compile';
 export default compile;
