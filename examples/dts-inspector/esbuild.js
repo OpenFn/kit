d359d3adb5291e430f05274fc07bb88c97c0e0be
Joe Clark <josephjclark@gmail.com>
2023-07-19 12:20:13 +0100

    update esbuild
    Note that this has broken the watch in describe-package. It's not a priority for right now

--- examples/dts-inspector/esbuild.js
+++ examples/dts-inspector/esbuild.js
@@ -1,29 +1,30 @@
-import liveserver from 'live-server' // dev server
-import esbuild from 'esbuild'
-import postcss from 'esbuild-postcss'
+import liveserver from 'live-server'; // dev server
+import esbuild from 'esbuild';
+import postcss from 'esbuild-postcss';
 
+// TODO it's now possible to do serve through esbuild
 liveserver.start({
   port: 8080, // Set the server port. Defaults to 8080.
   root: '.', // Set root directory that's being served. Defaults to cwd.
   open: false, // When false, it won't load your browser by default.
   wait: 500, // Waits for all changes, before reloading. Defaults to 0 sec.
   logLevel: 2, // 0 = errors only, 1 = some, 2 = lots
-  ignore: 'node_modules/**/src'
-})
+  ignore: 'node_modules/**/src',
+});
 
-await esbuild.build({
-  entryPoints: ['src/index.tsx', '@openfn/describe-package/worker', 'src/from-unpkg.tsx'],
+const context = await esbuild.context({
+  entryPoints: [
+    'src/index.tsx',
+    '@openfn/describe-package/worker',
+    'src/from-unpkg.tsx',
+  ],
   outdir: 'dist/',
   bundle: true,
   splitting: true,
   sourcemap: true,
-  format: "esm",
-  target: ["es2020"],
+  format: 'esm',
+  target: ['es2020'],
   plugins: [postcss()],
-  watch: {
-    onRebuild(error, result) {
-      if (error) console.error('watch build failed:', error)
-      else console.log('watch build succeeded:', result)
-    },
-  },
-})
+});
+
+await context.watch();
