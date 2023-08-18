01eb38cd4becf16f0a1eb4aa670c589fbc44903b
Zacck <coderv63@gmail.com>
2023-08-18 08:50:31 +0200

    Use correct function when building state

--- packages/deploy/src/index.ts
+++ packages/deploy/src/index.ts
@@ -27,7 +27,7 @@ function mergeDefaultOptions(options: Partial<DeployConfig>): DeployConfig {
   };
 }
 
-export { getProject, mergeSpecIntoState}; 
+export { getProject, mergeProjectPayloadIntoState}; 
 
 export async function getConfig(path?: string): Promise<DeployConfig> {
   try {
