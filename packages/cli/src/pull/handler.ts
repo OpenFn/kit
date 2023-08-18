0446e137f15eebf5de56bbac463abebc055cba9c
Taylor Downs <downs.taylor@gmail.com>
2023-08-18 13:53:07 +0300

    better logging for cli pull

--- packages/cli/src/pull/handler.ts
+++ packages/cli/src/pull/handler.ts
@@ -15,18 +15,37 @@ async function pullHandler(options: PullOptions, logger: Logger) {
   try {
     assertPath(options.projectId);
     const config = mergeOverrides(await getConfig(options.configPath), options);
-    logger.always('Downloading project yaml and  state from instance');
+    logger.always('Downloading project yaml and state from instance');
 
     const state = await getState(config.statePath);
     const url = new URL(
       `/download/yaml?id=${options.projectId}`,
       config.endpoint
     );
-    const res = await fetch(url);
+    logger.debug('Fetching project spec from ', url)
 
+    const res = await fetch(url, {
+      headers: {
+        Authorization: `Bearer ${config.apiKey}`,
+        Accept: 'application/json',
+      },
+    });
+
+    // TODO - what if the request was denied (406) or 404?
+
+    const resolvedPath = path.resolve(config.specPath);
+    logger.debug('reading spec from', resolvedPath)
     // @ts-ignore
-    await fs.writeFile(path.resolve(config.specPath), res.body!);
+    await fs.writeFile(resolvedPath, res.body!);
     const spec = await getSpec(config.specPath);
+    logger.debug('validated spec: ', spec)
+
+    if (spec.errors) {
+      logger.error('ERROR: invalid spec');
+      logger.error(spec.errors);
+      process.exitCode = 1;
+      process.exit(1);
+    }
 
     const nextState = mergeSpecIntoState(state, spec.doc);
     await fs.writeFile(
