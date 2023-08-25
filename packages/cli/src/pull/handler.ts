69b4d779c54959d1b0b38969aebc4dee9ef13658
Joe Clark <jclark@openfn.org>
2023-08-25 11:23:10 +0100

    cli: fix types, tidyup

--- packages/cli/src/pull/handler.ts
+++ packages/cli/src/pull/handler.ts
@@ -4,15 +4,12 @@ import {
   DeployConfig,
   getConfig,
   getProject,
-  mergeProjectPayloadIntoState,
-  mergeSpecIntoState,
   getSpec,
   getStateFromProjectPayload,
 } from '@openfn/deploy';
 import type { Logger } from '../util/logger';
 import { PullOptions } from '../pull/command';
 import assertPath from '../util/assert-path';
-import { option } from 'yargs';
 
 async function pullHandler(options: PullOptions, logger: Logger) {
   try {
@@ -40,36 +37,21 @@ async function pullHandler(options: PullOptions, logger: Logger) {
     logger.debug('reading spec from', resolvedPath);
 
     // Write the yaml down to disk
-    await fs.writeFile(resolvedPath, res.body!);
+    // @ts-ignore
+    await fs.writeFile(resolvedPath, res.body);
 
     // Read the spec back in a parsed yaml
     const spec = await getSpec(resolvedPath);
-    // logger.debug('validated spec: ', JSON.stringify(spec.doc, null, 2));
 
     // Get the latest project from Lightning
     // TODO - what if the request was denied (406) or 404?
     const { data: project } = await getProject(config, options.projectId);
-    console.log('the project from server:', JSON.stringify(project, null, 2));
-    const finalState = getStateFromProjectPayload(project);
-
-    // // Merge the spec into state
-    // // TODO Need to intialise new state with uuids from the project
-    // const finalState = mergeProjectPayloadIntoState(
-    //   getStateFromProjectPayload(project),
-    //   spec.doc
-    // );
-    console.log(JSON.stringify(finalState, null, 2));
-
-    // const finalState = mergeSpecIntoState(x, spec.doc);
-    // console.log(spec);
-    // console.log('newState ID:', JSON.stringify(newState.id));
-
-    // Now merge the project into the state
+    const state = getStateFromProjectPayload(project!);
 
     // And finally write the final, deployed state to disk
     await fs.writeFile(
       path.resolve(config.statePath),
-      JSON.stringify(finalState, null, 2)
+      JSON.stringify(state, null, 2)
     );
 
     if (spec.errors.length > 0) {
