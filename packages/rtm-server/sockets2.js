c0751f2ae9abe1f5f4c0205bbda4fb00c83830d3
Joe Clark <jclark@openfn.org>
2023-09-19 17:10:31 +0100

    rtm: remove some scratchpads

--- packages/rtm-server/sockets2.js
+++
@@ -1,8 +0,0 @@
-import WebSocket, { WebSocketServer } from 'ws';
-
-const s = new WebSocket('ws://localhost:8080');
-s.on('open', () => {
-  console.log('sending...');
-  s.send('hello');
-  process.exit(0);
-});
