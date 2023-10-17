2efbb06d7d6f7164ef145dc4302a0e8d4c81add2
Joe Clark <jclark@openfn.org>
2023-10-17 19:05:42 +0100

    worker-tests: add autoinstall test

--- integration-tests/worker/test/integration.test.ts
+++ integration-tests/worker/test/integration.test.ts
@@ -1,4 +1,5 @@
 import test from 'ava';
+import path from 'node:path';
 
 import createLightningServer from '@openfn/lightning-mock';
 
@@ -9,6 +10,7 @@ import createLogger, { createMockLogger } from '@openfn/logger';
 
 let lightning;
 let worker;
+let engine;
 
 test.afterEach(() => {
   lightning.destroy();
@@ -20,10 +22,11 @@ const initLightning = () => {
 };
 
 const initWorker = () => {
-  const engine = createEngine({
+  engine = createEngine({
     // logger: createLogger('engine', { level: 'debug' }),
     logger: createMockLogger(),
     runtimeLogger: createMockLogger(),
+    repoDir: path.resolve('./tmp/repo'),
   });
 
   worker = createWorkerServer(engine, {
@@ -63,6 +66,11 @@ test.only('should run a simple job with no compilation', (t) => {
   return new Promise((done) => {
     initLightning();
     lightning.on('attempt:complete', (evt) => {
+      // the complete payload should have a final dataclip id
+      // we should also be able to ask lightning for the result
+      // const { payload: state } = evt;
+      // console.log(evt);
+      // t.deepEqual(state, { data: { answer: 42 } });
       t.pass('completed attempt');
       done();
     });
@@ -79,3 +87,58 @@ test.only('should run a simple job with no compilation', (t) => {
     });
   });
 });
+
+// todo ensure repo is clean
+// check how we manage the env in cli tests
+test('run a job with autoinstall of common', (t) => {
+  return new Promise((done) => {
+    initLightning();
+
+    let autoinstallEvent;
+
+    lightning.on('attempt:complete', (evt) => {
+      try {
+        t.truthy(autoinstallEvent);
+        t.is(autoinstallEvent.module, '@openfn/language-common');
+        t.is(autoinstallEvent.version, 'latest');
+        t.assert(autoinstallEvent.duration >= 100);
+
+        const { result } = evt;
+        t.deepEqual(result, { data: { answer: 42 } });
+        done();
+      } catch (e) {
+        t.fail(e);
+        done();
+      }
+    });
+
+    initWorker();
+
+    // listen to events for this attempt
+    engine.listen('a1', {
+      'autoinstall-complete': (evt) => {
+        autoinstallEvent = evt;
+      },
+    });
+
+    lightning.enqueueAttempt({
+      id: 'a1',
+      jobs: [
+        {
+          id: 'j1',
+          adaptor: '@openfn/language-common@latest', // version lock to something stable?
+          body: 'fn(() => ({ data: { answer: 42} }))',
+        },
+      ],
+    });
+  });
+});
+
+// this depends on prior test!
+test.todo("run a job which doesn't autoinstall common");
+test.todo('run a job with complex behaviours (initial state, branching)');
+
+// maybe create a http server with basic auth on the endpoint
+// use http adaptor to call the server
+// obviously credential is lazy loaded
+test.todo('run a job which requires credentials');
