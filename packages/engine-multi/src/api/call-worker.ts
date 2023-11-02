bfcc95c297a10daf2d91dbbd33e84deeaa53e006
Joe Clark <jclark@openfn.org>
2023-11-02 18:18:30 +0000

    engine: increase destroy timeout

--- packages/engine-multi/src/api/call-worker.ts
+++ packages/engine-multi/src/api/call-worker.ts
@@ -65,7 +65,7 @@ export default function initWorkers(
 
     // Defer the return to allow workerpool to close down
     return new Promise((done) => {
-      setTimeout(done, 1);
+      setTimeout(done, 100);
     });
   };
 }
