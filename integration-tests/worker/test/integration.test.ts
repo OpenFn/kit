95514edc29e118167d7204020626c9be6d9bc33d
Joe Clark <jclark@openfn.org>
2023-11-02 18:30:58 +0000

    worker-tests: logging

--- integration-tests/worker/test/integration.test.ts
+++ integration-tests/worker/test/integration.test.ts
@@ -380,9 +380,11 @@ test('stateful adaptor should create a new client for each attempt', (t) => {
     let results = {};
 
     initLightning();
-
+    console.log('>> naughty test: 1)', attempt1.id);
+    console.log('>> naughty test: 2)', attempt2.id);
     lightning.on('attempt:complete', (evt) => {
       const id = evt.attemptId;
+      console.log('>>> compelted naughty test', id);
       results[id] = lightning.getResult(id);
 
       if (id === attempt2.id) {
