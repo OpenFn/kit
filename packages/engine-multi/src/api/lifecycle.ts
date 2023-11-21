dcc56c246393653c81ed0077cbb4f68219ce3533
Joe Clark <jclark@openfn.org>
2023-11-21 14:38:35 +0000

    engine: do not emit job logs to stdout

--- packages/engine-multi/src/api/lifecycle.ts
+++ packages/engine-multi/src/api/lifecycle.ts
@@ -118,16 +118,9 @@ export const log = (
   event: internalEvents.LogEvent
 ) => {
   const { threadId } = event;
-  // // TODO not sure about this stuff, I think we can drop it?
-  // const newMessage = {
-  //   ...message,
-  //   // Prefix the job id in all local jobs
-  //   // I'm sure there are nicer, more elegant ways of doing this
-  //   message: [`[${workflowId}]`, ...message.message],
-  // };
-  // TODO: if these are logs from within the runtime,
-  // should we use context.runtimeLogger ?
-  context.logger.proxy(event.message);
+
+  // Note: we do not log job stuff to stdout
+  // https://github.com/OpenFn/kit/issues/499
 
   context.emit(externalEvents.WORKFLOW_LOG, {
     threadId,
