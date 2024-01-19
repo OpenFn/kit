3909b4574fb8b92dcdcf274dcb0f4cdc45b634b4
Joe Clark <jclark@openfn.org>
2024-01-19 15:47:08 +0000

    tests: debug ci

--- integration-tests/worker/src/init.ts
+++ integration-tests/worker/src/init.ts
@@ -21,9 +21,12 @@ export const initWorker = async (
 ) => {
   const workerPort = randomPort();
 
-  const engineLogger = createMockLogger('engine', {
+  // const engineLogger = createMockLogger('engine', {
+  //   level: 'debug',
+  //   json: true,
+  // });
+  const engineLogger = createLogger('engine', {
     level: 'debug',
-    json: true,
   });
 
   const engine = await createEngine({
@@ -33,8 +36,8 @@ export const initWorker = async (
   });
 
   const worker = createWorkerServer(engine, {
-    logger: createMockLogger(),
-    // logger: createLogger('worker', { level: 'debug' }),
+    // logger: createMockLogger(),
+    logger: createLogger('worker', { level: 'debug' }),
     port: workerPort,
     lightning: `ws://localhost:${lightningPort}/worker`,
     secret: crypto.randomUUID(),
