d8e8ee3aa3af2bd05fb28af0a57888b597e34720
Joe Clark <jclark@openfn.org>
2024-01-31 12:33:02 +0000

    engine: types

--- packages/engine-multi/src/worker/thread/helpers.ts
+++ packages/engine-multi/src/worker/thread/helpers.ts
@@ -19,6 +19,7 @@ export const createLoggers = (
   publish?: any
 ) => {
   const log = (message: JSONLog) => {
+    // @ts-ignore
     publish(workerEvents.LOG, {
       workflowId,
       message: {
