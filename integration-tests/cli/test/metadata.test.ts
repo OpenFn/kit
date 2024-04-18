726e20845a1c1ca6b7dc07a9a20453be93125873
SatyamMattoo <satyammattoo2003@gmail.com>
2024-04-18 19:29:35 +0530

    added integration tests

--- integration-tests/cli/test/metadata.test.ts
+++ integration-tests/cli/test/metadata.test.ts
@@ -126,3 +126,18 @@ test.serial('does not log credentials', async (t) => {
   t.regex(logString, /(pin_number)/i);
   t.notRegex(logString, new RegExp(sensitiveValue), 'i');
 });
+
+// Test for auto-installing adaptor
+test.serial(
+  `openfn metadata -f -S "${state}" -a common --log-json --log info`,
+  async (t) => {
+    const { stdout } = await run(t.title);
+
+    t.regex(stdout, /Generating metadata/);
+    t.regex(stdout, /Installing packages.../);
+    t.regex(stdout, /Installed @openfn\/language-common@1.13.1/);
+    t.regex(stdout, /Installation complete in \d+\.\d+s/);
+    t.regex(stdout, /No metadata helper found/);
+    t.notRegex(stdout, /Returning metadata from cache/);
+  }
+);
