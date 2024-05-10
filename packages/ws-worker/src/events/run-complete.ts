75f90879676d2b20bc057b03ef2fec69575ad455
josephjclark <jclark@openfn.org>
2024-05-10 13:01:56 +0100

    Worker: ensure workers are released after an error on run:complete (#685)
    * worker: added logging
    
    Noisy but useful
    
    * worker: ensure run:complete returns even if the socket throws
    
    * changeset
    
    * worker: add a bit more logging on the channel
    
    * version: worker@1.1.7
    
    * worker: fix mock socket

--- packages/ws-worker/src/events/run-complete.ts
+++ packages/ws-worker/src/events/run-complete.ts
@@ -10,7 +10,7 @@ export default async function onWorkflowComplete(
   context: Context,
   _event: WorkflowCompletePayload
 ) {
-  const { state, channel, onFinish } = context;
+  const { state, channel, onFinish, logger } = context;
 
   // TODO I dont think the run final dataclip IS the last job dataclip
   // Especially not in parallelisation
@@ -19,10 +19,17 @@ export default async function onWorkflowComplete(
   const reason = calculateRunExitReason(state);
   await logFinalReason(context, reason);
 
-  await sendEvent<RunCompletePayload>(channel, RUN_COMPLETE, {
-    final_dataclip_id: state.lastDataclipId!,
-    ...reason,
-  });
+  try {
+    await sendEvent<RunCompletePayload>(channel, RUN_COMPLETE, {
+      final_dataclip_id: state.lastDataclipId!,
+      ...reason,
+    });
+  } catch (e) {
+    logger.error(
+      `${state.plan.id} failed to send ${RUN_COMPLETE} event. This run will be lost!`
+    );
+    logger.error(e);
+  }
 
   onFinish({ reason, state: result });
 }
