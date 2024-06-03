5c4b73d02a2eed89ebd56dc23df6f955609c082e
Joe Clark <jclark@openfn.org>
2024-06-03 12:57:39 +0100

    run pnpm format

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import runInChildProcess from './process/spawn';
-import {cmd} from './cli';
+import { cmd } from './cli';
 
 const opts = cmd.parseSync();
 runInChildProcess(opts);
