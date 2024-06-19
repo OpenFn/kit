49043bedca4aa742def730649d6fa6f4c1d2b687
Joe Clark <jclark@openfn.org>
2024-06-19 17:47:55 +0100

    tests: reduce specifity

--- integration-tests/worker/test/integration.test.ts
+++ integration-tests/worker/test/integration.test.ts
@@ -78,7 +78,6 @@ test.serial('run a job with autoinstall of common', (t) => {
       try {
         t.truthy(autoinstallEvent);
         t.is(autoinstallEvent.module, '@openfn/language-common');
-        t.is(autoinstallEvent.version, '1.14.0');
         // Expect autoinstall to take several seconds
         t.assert(autoinstallEvent.duration >= 1000);
 
