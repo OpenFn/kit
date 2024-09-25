f581c6b048fa858fd5c2715e4f37b34ccbaccffe
josephjclark <jclark@openfn.org>
2024-09-25 12:58:59 +0100

    Better worker logs (#784)
    * worker: added simple logging
    
    * log run times
    
    * changeset
    
    * format
    
    * log capacity
    
    * updates
    
    * format

--- packages/ws-worker/src/server.ts
+++ packages/ws-worker/src/server.ts
@@ -181,6 +181,7 @@ function createServer(engine: RuntimeEngine, options: ServerOptions = {}) {
   app.execute = async ({ id, token }: ClaimRun) => {
     if (app.socket) {
       try {
+        const start = Date.now();
         app.workflows[id] = true;
 
         const {
@@ -197,7 +198,10 @@ function createServer(engine: RuntimeEngine, options: ServerOptions = {}) {
 
         // Callback to be triggered when the work is done (including errors)
         const onFinish = () => {
-          logger.debug(`workflow ${id} complete: releasing worker`);
+          const duration = (Date.now() - start) / 1000;
+          logger.debug(
+            `workflow ${id} complete in ${duration}s: releasing worker`
+          );
           delete app.workflows[id];
           runChannel.leave();
 
