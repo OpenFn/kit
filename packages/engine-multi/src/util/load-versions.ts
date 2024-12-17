fb670c037c151f381f4570c768c066c1ac4b10d2
Joe Clark <jclark@openfn.org>
2024-12-17 17:45:43 +0000

    engine: update package json importer

--- packages/engine-multi/src/util/load-versions.ts
+++ packages/engine-multi/src/util/load-versions.ts
@@ -3,21 +3,35 @@ import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { Versions } from '../types';
 
-const pkg = JSON.parse(
-  fs.readFileSync(
-    path.join(fileURLToPath(import.meta.url), '../../package.json'),
-    'utf-8'
-  )
-);
+let pkg: any;
 
-// Load key versions at init time
-const versions = {
-  node: process.version.substring(1),
-  engine: pkg.version,
-  compiler: pkg.dependencies?.['@openfn/compiler'],
-  runtime: pkg.dependencies?.['@openfn/runtime'],
-};
+function getPkg(): any {
+  if (!pkg) {
+    let nextPath = path.dirname(fileURLToPath(import.meta.url));
+    while (nextPath) {
+      const pkgPath = path.resolve(nextPath, 'package.json');
+      try {
+        fs.statSync(pkgPath);
+        nextPath = pkgPath;
+        break;
+      } catch (e) {
+        nextPath = path.dirname(nextPath);
+      }
+    }
+
+    pkg = JSON.parse(fs.readFileSync(nextPath, 'utf-8'));
+  }
+  return pkg;
+}
 
 // Return a shallow clone of versions
 // because each workflow will scribble to it
-export default (): Versions => ({ ...versions });
+export default (): Versions => {
+  const pkg = getPkg();
+  return {
+    node: process.version.substring(1),
+    engine: pkg.version,
+    compiler: pkg.dependencies?.['@openfn/compiler'],
+    runtime: pkg.dependencies?.['@openfn/runtime'],
+  };
+};
