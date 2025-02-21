35aca25993c3e585e59412fff4aff1369f99af8a
Joe Clark <jclark@openfn.org>
2025-02-21 17:29:00 +0000

    lint

--- packages/ws-worker/src/server.ts
+++ packages/ws-worker/src/server.ts
@@ -138,7 +138,7 @@ function connect(app: ServerApp, logger: Logger, options: ServerOptions = {}) {
   // handles messages for the worker:queue
   const onMessage = (event: string) => {
     if (event === WORK_AVAILABLE) {
-      claim(app, logger, { maxWorkers: options.maxWorkflows }).catch((e) => {
+      claim(app, logger, { maxWorkers: options.maxWorkflows }).catch(() => {
         // do nothing - it's fine if  claim throws here
       });
     }
