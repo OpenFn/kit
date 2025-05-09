c55e41307633ec3a06c949e42f13e1018d752e4c
Joe Clark <jclark@openfn.org>
2025-05-09 16:06:55 +0100

    cli: allow execute from project and workflow.yaml
    pretty flaky but it works

--- packages/cli/src/util/validate-plan.ts
+++ packages/cli/src/util/validate-plan.ts
@@ -23,7 +23,12 @@ const assertWorkflowStructure = (plan: ExecutionPlan, logger: Logger) => {
   assertOptionsStructure(options, logger);
 };
 
+// TODO do we really need this stuff? I'm not wild about it
+// It is useful for manual workflow construction
+// skipped for now just while I get things working
 const assertStepStructure = (step: Job | Trigger, index: number) => {
+  return;
+
   const allowedKeys = [
     'id',
     'name',
--- packages/project/src/index.ts
+++ packages/project/src/index.ts
@@ -1,3 +1,5 @@
 import { Project } from './Project';
 
 export default Project;
+
+export { yamlToJson, jsonToYaml } from './util/yaml';
