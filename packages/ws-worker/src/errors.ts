fcb3fd54b6e610d301a8b3e375bfc32ac16317f4
Frank Midigo <midigofrank@gmail.com>
2025-06-30 13:50:08 +0300

    have a function to serialize error messages

--- packages/ws-worker/src/errors.ts
+++ packages/ws-worker/src/errors.ts
@@ -1,13 +1,29 @@
+function serializeMessage(message: any): string {
+  if (typeof message === 'string') {
+    return message;
+  }
+
+  if (message instanceof Error) {
+    return message.toString();
+  }
+
+  if (message && typeof message === 'object') {
+    try {
+      return JSON.stringify(message);
+    } catch {
+      return String(message);
+    }
+  }
+
+  return String(message);
+}
+
 export class LightningSocketError extends Error {
   name = 'LightningSocketError';
   event = '';
   rejectMessage = '';
   constructor(event: string, message: any) {
-    super(
-      `[${event}] ${
-        typeof message === 'string' ? message : JSON.stringify(message)
-      }`
-    );
+    super(`[${event}] ${serializeMessage(message)}`);
     this.event = event;
     this.rejectMessage = message;
   }
