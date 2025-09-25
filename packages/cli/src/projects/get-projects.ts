6eecacf5833249d0519eb2c573c5bc9d0b0a220b
Farhan Yahaya <yahyafarhan48@gmail.com>
2025-09-25 09:31:07 +0000

    chore: extract get-project functionality

---
+++ packages/cli/src/projects/get-projects.ts
@@ -0,0 +1,50 @@
+import { Logger } from '../util';
+import path from 'path';
+import fs from 'fs';
+import readHead from '../util/read-head';
+
+interface FsProject {
+  id: string;
+  name: string;
+  fileName: string;
+  active?: boolean;
+}
+
+export default async function getFsProjects(projectsDir: string, logger: Logger) {
+  const projects = fs
+    .readdirSync(projectsDir)
+    .filter((name) => ['.yaml', '.yml'].includes(path.extname(name)));
+  // for each yml file here, cat the head and find the name
+  const foundProjects: FsProject[] = [];
+  for (const p of projects) {
+    // get id and name
+    const content = await readHead(path.join(projectsDir, p));
+    const id = getYamlItem(content, 'id');
+    const name = getYamlItem(content, 'name');
+    if (id && name) {
+      foundProjects.push({ id, name, fileName: p });
+    }
+  }
+
+  if (!foundProjects.length) return [];
+
+  let active: string | undefined;
+  const activeProjectYaml = path.join(path.dirname(projectsDir), 'openfn.yaml');
+  if (
+    fs.existsSync(activeProjectYaml) &&
+    fs.statSync(activeProjectYaml).isFile()
+  ) {
+    active = getYamlItem(await readHead(activeProjectYaml), 'name');
+  } else {
+    logger.warn('No active project found');
+  }
+
+  return foundProjects.map((f) =>
+    f.name === active ? { ...f, active: true } : f
+  );
+}
+
+function getYamlItem(content: string, key: string) {
+  const rE = new RegExp(`^${key}:.+`, 'gm');
+  return content.match(rE)?.[0]?.split(':')?.[1].trim();
+}
--- packages/cli/src/projects/handler.ts
+++ packages/cli/src/projects/handler.ts
@@ -2,7 +2,7 @@ import type { ProjectsOptions } from './command';
 import type { Logger } from '../util/logger';
 import path from 'path';
 import fs from 'fs';
-import readHead from '../util/read-head';
+import getFsProjects from './get-projects';
 
 const projectsHandler = async (options: ProjectsOptions, logger: Logger) => {
   const commandPath = options.projectPath ?? process.cwd();
@@ -12,47 +12,16 @@ const projectsHandler = async (options: ProjectsOptions, logger: Logger) => {
     logger.error('.projects folder not found');
     return;
   }
-  const projects = fs
-    .readdirSync(projectsDir)
-    .filter((name) => ['.yaml', '.yml'].includes(path.extname(name)));
-  // for each yml file here, cat the head and find the name
-  const foundProjects: { id: string; name: string }[] = [];
-  for (const p of projects) {
-    // get id and name
-    const content = await readHead(path.join(projectsDir, p));
-    const id = getYamlItem(content, 'id');
-    const name = getYamlItem(content, 'name');
-    if (id && name) {
-      foundProjects.push({ id, name });
-    }
-  }
-
-  if(!foundProjects.length){
-    logger.error('No openfn project available');
-    return;
-  }
-
-  let active: string | undefined;
-  const activeProjectYaml = path.join(commandPath, 'openfn.yaml');
-  if (
-    fs.existsSync(activeProjectYaml) &&
-    fs.statSync(activeProjectYaml).isFile()
-  ) {
-    active = getYamlItem(await readHead(activeProjectYaml), 'name');
-  }else{
-    logger.warn('No active project found')
+  const projects = await getFsProjects(projectsDir, logger);
+  if (!projects.length) {
+    logger.error('No openfn projects available');
   }
 
   process.stdout.write(
-    `Available openfn projects\n\n${foundProjects
-      .map((p) => p.name + (p.name === active ? ' (active)' : ''))
+    `Available openfn projects\n\n${projects
+      .map((p) => p.name + (p.active ? ' (active)' : ''))
       .join('\n')}\n\n`
   );
 };
 
 export default projectsHandler;
-
-function getYamlItem(content: string, key: string) {
-  const rE = new RegExp(`^${key}:.+`, 'gm');
-  return content.match(rE)?.[0]?.split(':')?.[1].trim();
-}
