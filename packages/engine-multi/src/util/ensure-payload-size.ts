96991314d67a5dbe384d5123644ca1a072f469d2
Joe Clark <jclark@openfn.org>
2025-10-20 10:40:37 +0100

    ensure final_state payloads are properly redacted

--- packages/engine-multi/src/util/ensure-payload-size.ts
+++ packages/engine-multi/src/util/ensure-payload-size.ts
@@ -1,11 +1,14 @@
-export const REDACTED_STATE = {
-  data: '[REDACTED_STATE]',
-  _$REDACTED$_: true,
-};
+// This specifies which keys of an event payload to potentially redact
+// if they are too big
+const KEYS_TO_VERIFY = ['state', 'final_state', 'log'];
 
-export const REDACTED_LOG = {
-  message: ['[REDACTED: Message length exceeds payload limit]'],
-  _$REDACTED$_: true,
+const replacements: Record<string, any> = {
+  log: {
+    message: ['[REDACTED: Message length exceeds payload limit]'],
+  },
+  default: {
+    data: '[REDACTED]',
+  },
 };
 
 export const verify = (value: any, limit_mb: number = 10) => {
@@ -32,19 +35,14 @@ export const verify = (value: any, limit_mb: number = 10) => {
 export default (payload: any, limit_mb: number = 10) => {
   const newPayload = { ...payload };
 
-  // The payload could be any of the runtime events
-  // The bits we might want to redact are state and message
-  try {
-    verify(payload.state, limit_mb);
-  } catch (e) {
-    newPayload.state = REDACTED_STATE;
-    newPayload.redacted = true;
+  for (const key of KEYS_TO_VERIFY) {
+    try {
+      verify(payload[key], limit_mb);
+    } catch (e) {
+      Object.assign(newPayload[key], replacements[key] ?? replacements.default);
+      newPayload.redacted = true;
+    }
   }
-  try {
-    verify(payload.log, limit_mb);
-  } catch (e) {
-    Object.assign(newPayload.log, REDACTED_LOG);
-    newPayload.redacted = true;
-  }
+
   return newPayload;
 };
