5dc9dd4fdbed139fc4f06d2f9484aa4f6fb61270
Joe Clark <jclark@openfn.org>
2025-11-27 09:12:15 +0000

    tidying

--- packages/project/src/parse/from-json.ts
+++
@@ -1,9 +0,0 @@
-// this function has the job of taking an abitrary chunk of JSON
-// and working out if it's state v1 or project v2 file
-export default (
-  state: JSON // well it'll be state or project won't it
-  // I don't think we ever need options here?
-) => {
-  // if this looks like state v1, return from app state
-  // if htis looks like state v2
-};
--- packages/project/src/parse/from-project.ts
+++ packages/project/src/parse/from-project.ts
@@ -4,7 +4,6 @@ import Project from '../Project';
 import ensureJson from '../util/ensure-json';
 import { Provisioner } from '@openfn/lexicon/lightning';
 import fromAppState from './from-app-state';
-import slugify from '../util/slugify';
 
 // Load a project from any JSON or yaml representation
 // This is backwards-compatible with v1 state.json files
@@ -20,9 +19,6 @@ export type SerializedWorkflow = {
   id: string;
   name: string;
 
-  // jobs: SerializedJob[];
-  // triggers: SerializedTrigger[];
-  // edges: SerializedEdge[];
   steps: l.Step[];
 
   openfn?: l.ProjectMeta;
@@ -51,96 +47,10 @@ const from_v1 = (data: Provisioner.Project) => {
   return fromAppState(data);
 };
 
-// Actually we should serialize the workflow using lighting's
-// structure
-// Let's do that here in the state file now, so it's like v1
-// Let's work out the expanded workflow.yaml file later
 const from_v2 = (data: SerializedProject) => {
   // nothing to do
   // (When we add v3, we'll ned to migrate through this)
   return {
     ...data,
-    // workflows: data.workflows.map(mapWorkflow),
   };
-};
-
-// This maps project workflows, which broadly follow the provisioner
-// structure, into the internal runtime workflow structures
-// TODO this probably needs its own set of unit tests
-export const mapWorkflow = (workflow: SerializedWorkflow) => {
-  const { jobs, edges, triggers } = workflow;
-
-  const mapped: l.Workflow = {
-    id: workflow.id ?? slugify(workflow.name),
-    name: workflow.name,
-    steps: [],
-    openfn: workflow.openfn,
-  };
-
-  triggers.forEach((trigger) => {
-    const connectedEdges = edges.filter(
-      (e) => e.source_trigger_id === trigger.id
-    );
-    mapped.steps.push({
-      id: trigger.id ?? trigger.type ?? 'trigger',
-      type: trigger.type,
-      openfn: trigger.openfn,
-      next: connectedEdges.reduce((obj: any, edge) => {
-        const target = jobs.find((j) => j.id === edge.target_job_id);
-        if (!target) {
-          throw new Error(`Failed to find ${edge.target_job_id}`);
-        }
-        // we use the name, not the id, to reference
-        obj[slugify(target.name)] = mapTriggerEdgeCondition(edge);
-        return obj;
-      }, {}),
-    } as l.Trigger);
-  });
-
-  jobs.forEach((step) => {
-    const outboundEdges = edges.filter(
-      (e) => e.source_job_id === step.id || e.source_trigger_id === step.id
-    );
-
-    const { body: expression, name, adaptor } = step;
-
-    const s: any = {
-      id: slugify(name),
-      name: name,
-      expression,
-      adaptor,
-      openfn: step.openfn,
-    };
-
-    if (outboundEdges.length) {
-      s.next = outboundEdges.reduce((next, edge) => {
-        const target = jobs.find((j) => j.id === edge.target_job_id);
-        // @ts-ignore
-        next[slugify(target.name)] = mapTriggerEdgeCondition(edge);
-        return next;
-      }, {});
-    }
-    mapped.steps.push(s);
-  });
-
-  return mapped;
-};
-
-const mapTriggerEdgeCondition = (edge: SerializedEdge) => {
-  const e: any = {
-    disabled: !edge.openfn?.enabled,
-  };
-  if (edge.condition_type === 'always') {
-    e.condition = true;
-  } else if (edge.condition_type === 'never') {
-    e.condition = false;
-  } else {
-    e.condition = edge.condition_expression;
-  }
-
-  // Do this last so that it serializes last
-  e.openfn = {
-    uuid: edge.id,
-  };
-  return e;
 };
