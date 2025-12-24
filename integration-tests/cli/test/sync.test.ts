4742e18f28779132b25c55c75b156f358fc6222c
Joe Clark <jclark@openfn.org>
2025-12-24 18:24:17 +0100

    add one passing integration test

--- integration-tests/cli/test/sync.test.ts
+++ integration-tests/cli/test/sync.test.ts
@@ -1,110 +1,44 @@
 import test from 'ava';
-import { MockAgent, setGlobalDispatcher } from 'undici';
+import path from 'node:path';
+import fs from 'node:fs';
 import run from '../src/run';
 import { generateProject } from '@openfn/project';
+import createLightningServer from '@openfn/lightning-mock';
+import { rimraf } from 'rimraf';
 
-// quite a lot I need to do here
-// I need to easily create projects with gen project
-// i need a file system to pull to
-// i need a mock server
+let PORT = 5353;
+let lightning;
+const endpoint = `http://localhost:${PORT}/api/provision`;
 
-// this should all be against v2 files
+test.before(async () => {
+  await rimraf('tmp/sync');
 
-// set up a mock server
-// with some data
-let mockAgent = new MockAgent();
-mockAgent.disableNetConnect();
-setGlobalDispatcher(mockAgent);
+  lightning = createLightningServer({
+    port: PORT,
+  });
+});
 
-let serverid = 0;
-
-const createMockServer = (data) => {
-  let projects = {};
-  projects[data.id] = data;
-
-  const endpoint = `http://lightning-${++serverid}`;
-
-  const getProject = (id) => projects[id];
-  const setProject = (data) => {
-    projects[data.id] = data;
-  };
-  const mockPool = mockAgent.get(endpoint);
-  mockPool
-    .intercept({
-      path: /api\/provision\/(.+)/,
-      method: 'GET',
-    })
-    .reply((args) => {
-      const id = args.path.split('/').at(-1);
-      const data = getProject(id);
-      return {
-        data,
-        statusCode: 200,
-        headers: { 'content-type': 'application/json' },
-      };
-    })
-    .persist();
-
-  mockPool
-    .intercept({
-      path: /api\/provision/,
-      method: 'POST',
-    })
-    .reply((args) => {
-      const data = JSON.parse(args.body);
-      setProject(data);
-      return {
-        statusCode: 200,
-      };
-    })
-    .persist();
+const initWorkspace = (t: any) => {
+  const id = t.title.replaceAll(' ', '_').toLowerCase();
+  const p = path.resolve('tmp/sync', id);
 
   return {
-    add: setProject,
-    endpoint,
-    destroy: () => {
-      mockPool.destroy();
+    workspace: p,
+    read: (filePath: string) => {
+      return fs.readFileSync(path.resolve(p, filePath), 'utf8');
     },
   };
 };
 
-test('server test', async (t) => {
-  const { endpoint, destroy } = createMockServer({ id: 'x' });
-  const response = await fetch(`${endpoint}/api/provision/x`);
-  const project = await response.json();
-  t.deepEqual(project, { id: 'x' });
-
-  await fetch(`${endpoint}/api/provision`, {
-    method: 'POST',
-    body: JSON.stringify({ id: 'x', name: 'test' }),
-    headers: {
-      'content-type': 'application-json',
-    },
-  });
-
-  const response2 = await fetch(`${endpoint}/api/provision/x`);
-  const project2 = await response2.json();
-  t.deepEqual(project2, { id: 'x', name: 'test' });
-  destroy();
-});
-
-test.skip('fetch a project', async (t) => {
-  const workspace = 'tmp/a';
-
+test('fetch a project', async (t) => {
+  const { workspace, read } = initWorkspace(t);
   // generate a project
   const project = generateProject('patients', ['trigger-job'], {
     openfnUuid: true,
   });
-  const state = project.serialize('state');
-  // get its statefile
-  // add to the server
-  const { endpoint, destroy, add } = createMockServer({ id: 'x' });
-  add(state);
+  const state = project.serialize('state', { format: 'json' });
+  lightning.addProject(state);
 
-  // now run cli fetch
-  // this fails because its running out of process, so the mock server
-  // isnt running
-  // i think?
   const { stdout } = await run(
     `openfn project fetch \
        --workspace ${workspace} \
@@ -114,8 +48,9 @@ test.skip('fetch a project', async (t) => {
        ${project.openfn.uuid}`
   );
 
-  console.log(stdout);
   // now check that the filesystem is roughly right
+  const pyaml = read('.projects/main@localhost.yaml');
 
-  destroy();
+  t.regex(pyaml, /id: patients/);
+  t.regex(pyaml, new RegExp(`uuid: ${project.openfn.uuid}`));
 });
