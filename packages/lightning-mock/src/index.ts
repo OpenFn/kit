1ef134dd3532f2d52525e766753cfcdbe2ae64b8
Joe Clark <jclark@openfn.org>
2025-12-25 16:47:11 +0100

    export default project id for testing

--- packages/lightning-mock/src/index.ts
+++ packages/lightning-mock/src/index.ts
@@ -2,3 +2,5 @@ import createLightningServer from './server';
 export default createLightningServer;
 
 export { toBase64, generateKeys } from './util';
+
+export { DEFAULT_PROJECT_ID } from './api-rest';
