dcd67ada4499a763fa4f314a99f16f907a8d3551
Joe Clark <jclark@openfn.org>
2026-01-19 11:58:13 +0000

    remove another needless destroy

--- packages/runtime/src/util/ensure-state-size.ts
+++ packages/runtime/src/util/ensure-state-size.ts
@@ -25,7 +25,6 @@ export default async (value: any, limit_mb: number = 500) => {
       size_bytes += Buffer.byteLength(chunk, 'utf8');
 
       if (size_bytes > limitBytes) {
-        stream.destroy();
         throw new StateTooLargeError(limit_mb);
       }
     }
