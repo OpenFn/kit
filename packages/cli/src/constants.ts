25b87e772a1d54f2f73b94f50a83f0a8856d4f73
Farhan Yahaya <yahyafarhan48@gmail.com>
2026-03-24 16:11:37 +0000

    chore: update default repo-dir

--- packages/cli/src/constants.ts
+++ packages/cli/src/constants.ts
@@ -1,1 +1,5 @@
-export const DEFAULT_REPO_DIR = '/tmp/openfn/repo';
+import * as os from 'node:os';
+import * as path from 'node:path';
+const homeDir = os.homedir();
+
+export const DEFAULT_REPO_DIR = path.join(homeDir, '/.openfn/repo');
