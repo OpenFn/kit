ea4818da421c53c68c05df0d886fdf5ac3845078
Joe Clark <jclark@openfn.org>
2026-03-27 16:18:44 +0000

    runtime: tweak linker to prefer ESM support

---
+++ .changeset/plain-taxis-prove.md
@@ -0,0 +1,6 @@
+---
+'@openfn/runtime': minor
+---
+
+- Enable full compatibility with node 24
+- When loading modules, prefer ESM targets over CJS targets
--- packages/runtime/src/modules/repo.ts
+++ packages/runtime/src/modules/repo.ts
@@ -231,27 +231,10 @@ export const getModuleEntryPoint = async (
       'utf8'
     );
     const pkg = JSON.parse(pkgRaw);
-    let main = 'index.js';
 
-    // TODO Turns out that importing the ESM format actually blows up
-    // (at least when we try to import lodash)
-    // if (pkg.exports) {
-    //   if (typeof pkg.exports === 'string') {
-    //     main = pkg.exports;
-    //   } else {
-    //     const defaultExport = pkg.exports['.']; // TODO what if this doesn't exist...
-    //     if (typeof defaultExport == 'string') {
-    //       main = defaultExport;
-    //     } else {
-    //       main = defaultExport.import;
-    //     }
-    //   }
-    // } else
-    // Safer for now to just use the CJS import
-    if (pkg.main) {
-      main = pkg.main;
-    }
+    const main = pkg.exports?.['.']?.import ?? pkg.main ?? 'index.js';
     const p = path.resolve(moduleRoot, main);
+
     return { path: p, version: pkg.version };
   }
   return null;
