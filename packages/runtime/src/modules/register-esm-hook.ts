80a75a0053df2dcd5d3846e931534f6f911fb205
Joe Clark <jclark@openfn.org>
2026-03-31 17:11:07 +0100

    add a custom module loader to handle older imports without .js extensions

---
+++ packages/runtime/src/modules/register-esm-hook.ts
@@ -0,0 +1,7 @@
+import { register } from 'node:module';
+
+// Backcompat loader hook for ESM modules that import CJS subpaths without a .js extension
+// e.g. `import set from 'lodash/set'` instead of `import set from 'lodash/set.js'`
+export const registerEsmHook = () => {
+  register(new URL('./esm-resolve-hook.js', import.meta.url));
+};
