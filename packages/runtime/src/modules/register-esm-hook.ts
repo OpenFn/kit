e100a4b7a1d16d5c43a5864946d1574479a7875c
Joe Clark <jclark@openfn.org>
2026-03-31 17:39:49 +0100

    fix module loader path

--- packages/runtime/src/modules/register-esm-hook.ts
+++ packages/runtime/src/modules/register-esm-hook.ts
@@ -3,5 +3,5 @@ import { register } from 'node:module';
 // Backcompat loader hook for ESM modules that import CJS subpaths without a .js extension
 // e.g. `import set from 'lodash/set'` instead of `import set from 'lodash/set.js'`
 export const registerEsmHook = () => {
-  register(new URL('./esm-resolve-hook.js', import.meta.url));
+  register(new URL('./modules/esm-resolve-hook.js', import.meta.url));
 };
