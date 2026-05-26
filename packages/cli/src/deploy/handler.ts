4ce4c11f8686d4af0486f97cfe61e2595de43464
Joe Clark <jclark@openfn.org>
2026-05-26 15:55:39 +0100

    remove log

--- packages/cli/src/deploy/handler.ts
+++ packages/cli/src/deploy/handler.ts
@@ -34,7 +34,6 @@ async function deployHandler(
   }
 
   try {
-    console.log({ options });
     const config = mergeOverrides(await getConfig(options.configPath), options);
 
     const v2ConfigPath = path.join(
