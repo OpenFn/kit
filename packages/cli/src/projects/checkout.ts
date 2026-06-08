9c0594903c5637ee65c78e8ef271ffbc1f9f8922
Joe Clark <jclark@openfn.org>
2026-06-08 15:48:27 +0100

    types

--- packages/cli/src/projects/checkout.ts
+++ packages/cli/src/projects/checkout.ts
@@ -1,5 +1,5 @@
 import yargs from 'yargs';
-import Project, { versionsEqual, Workspace } from '@openfn/project';
+import Project, { Workspace } from '@openfn/project';
 import path from 'path';
 import fs from 'fs';
 import { rimraf } from 'rimraf';
